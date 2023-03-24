dfcd240129f026a5ed8925178a0c0ffa0acf3027
Jordan Bean <jordanbean@microsoft.com>
2023-03-24 17:26:51 -0500

    modules

---
+++ src/Modules/TeamsAuthentication/TeamsAuthentication.psm1
@@ -0,0 +1,31 @@
+function Get-TeamsAccessTokens {
+  $clientSecret = $env:AzureAdClientSecret
+  $applicationId = $env:AzureAdClientId
+  $tenantId = $env:AzureAdTenantId
+
+  $graphTokenBody = @{   
+    Grant_Type    = "client_credentials"   
+    Scope         = "https://graph.microsoft.com/.default"   
+    Client_Id     = $applicationId
+    Client_Secret = $clientSecret   
+  }
+
+  Write-Information "Retriving Graph token..."
+
+  $graphToken = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -Method POST -Body $graphTokenBody | Select-Object -ExpandProperty Access_Token 
+
+  $teamsTokenBody = @{   
+    Grant_Type    = "client_credentials"   
+    Scope         = "48ac35b8-9aa8-4d74-927d-1f4a14a0b239/.default"   
+    Client_Id     = $applicationId   
+    Client_Secret = $clientSecret 
+  } 
+
+  $teamsToken = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -Method POST -Body $teamsTokenBody | Select-Object -ExpandProperty Access_Token
+
+  Write-Information "Retrieved Graph token"
+
+  return @($graphToken, $teamsToken)
+}
+
+Export-ModuleMember -Function Get-TeamsAccessTokens
--- src/ProvisionTeamsDID/run.ps1
+++ src/ProvisionTeamsDID/run.ps1
@@ -2,61 +2,13 @@ param([object] $serviceBusProvisionTeamsDiDMessage, $TriggerMetadata)
 
 $ErrorActionPreference = "Stop"
 
-Write-Information $serviceBusProvisionTeamsDiDMessage.UserPrincipalName
-Write-Information $serviceBusProvisionTeamsDiDMessage.TeamName
-Write-Information $serviceBusProvisionTeamsDiDMessage.ChannelName
+Import-Module TeamsAuthentication
+Import-Module TeamsAutomation
 
-$clientSecret = $env:AzureAdClientSecret
-$applicationId = $env:AzureAdClientId
-$tenantId = $env:AzureAdTenantId
+$graphToken, $teamsToken = Get-TeamsAccessTokens
 
-$graphTokenBody = @{   
-  Grant_Type    = "client_credentials"   
-  Scope         = "https://graph.microsoft.com/.default"   
-  Client_Id     = $applicationId
-  Client_Secret = $clientSecret   
-}
-
-Write-Information "Retriving Graph token..."
-
-$graphToken = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -Method POST -Body $graphTokenBody | Select-Object -ExpandProperty Access_Token 
-
-$teamsTokenBody = @{   
-  Grant_Type    = "client_credentials"   
-  Scope         = "48ac35b8-9aa8-4d74-927d-1f4a14a0b239/.default"   
-  Client_Id     = $applicationId   
-  Client_Secret = $clientSecret 
-} 
-
-$teamsToken = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -Method POST -Body $teamsTokenBody | Select-Object -ExpandProperty Access_Token
-
-Write-Information "Retrieved Graph token"
-
-Write-Information "Connecting to Microsoft Teams API..."
-
-Connect-MicrosoftTeams -AccessTokens @("$graphToken", "$teamsToken")
-
-Write-Information "Connected to Microsoft Teams API"
-
-Write-Information "Creating team $(${serviceBusProvisionTeamsDiDMessage}.TeamName)..."
-
-$group = New-Team -DisplayName $serviceBusProvisionTeamsDiDMessage.TeamName `
-  -Visibility Public `
-  -Owner $serviceBusProvisionTeamsDiDMessage.UserPrincipalName
-
-Write-Information "Created team $(${serviceBusProvisionTeamsDiDMessage}.TeamName)"
-
-Write-Information "Adding user $(${serviceBusProvisionTeamsDiDMessage}.UserPrincipalName) as owner..."
-
-Add-TeamUser -GroupId $group.GroupId `
-  -User $serviceBusProvisionTeamsDiDMessage.UserPrincipalName `
-  -Role Owner
-
-Write-Information "Added user $(${serviceBusProvisionTeamsDiDMessage}.UserPrincipalName) as owner"
-
-Write-Information "Creating channel $(${serviceBusProvisionTeamsDiDMessage}.ChannelName)..."
-
-New-TeamChannel -GroupId $group.GroupId `
-  -DisplayName $serviceBusProvisionTeamsDiDMessage.ChannelName
-
-Write-Information "Created channel $(${serviceBusProvisionTeamsDiDMessage}.ChannelName)"
+New-TeamsItems -GraphToken $graphToken `
+  -TeamsToken $teamsToken `
+  -UserPrincipalName $serviceBusProvisionTeamsDiDMessage.UserPrincipalName `
+  -TeamName $serviceBusProvisionTeamsDiDMessage.TeamName `
+  -ChannelName $serviceBusProvisionTeamsDiDMessage.ChannelName
